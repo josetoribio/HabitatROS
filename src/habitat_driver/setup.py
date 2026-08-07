77c560d1a7f2a213aa7359ddd1161475cf38114f
JoseToribio <joseftoribio@yahoo.com>
2026-08-07 09:51:23 +0000

    changed ema_node name

--- src/habitat_driver/setup.py
+++ src/habitat_driver/setup.py
@@ -33,7 +33,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'sht41_node = habitat_driver.sht41_node:main','humidity_node = habitat_driver.humidity_node:main',
+            'sht41_node = habitat_driver.sht41_node:main','ema_node = habitat_driver.humidity_node:main',
         ],
     },
 )
